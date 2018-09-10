e1135a430f54b931133e0a27db8bfa6ab074ca2d
Travis Tokuyama <travistokuyama@gmail.com>
2018-09-10 16:51:43 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,6 +8,9 @@ WIN_COMBINATIONS = [
   [0,1,2], # Top row
   [3,4,5], # Middle row
   [6,7,8], # Bottom row
-  [0,4,8], # Back slash diagnal
-  
+  [0,3,6], # Left column
+  [1,4,7], # Middle column
+  [2,5,8], # Right column
+  [0,4,8], # Back slash diagonal
+  [2,4,6] # Forward slash diagonal
 ]
