db13f14b4add8c21b51ded427836625a47500846
Travis Tokuyama <travistokuyama@gmail.com>
2018-09-10 16:59:58 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,7 @@ WIN_COMBINATIONS = [
   [2,4,6] # Forward slash diagonal
 ]
 
-def won(board)
+def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
   win_index_1 = win_combination[0]
   win_index_2 = win_combination[1]
